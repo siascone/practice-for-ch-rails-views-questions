314a1cc4efb81f584cd343e5924279e1913dc49f
siascone <spencer.iascone@gmail.com>
2023-06-16 10:15:27 -0600

    Test answers CRUD. Currently rendering json

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -20,14 +20,16 @@ class AnswersController < ApplicationController
     
     def new
         @answer = Answer.new
-        render :new
+        render json: "You need to build an new.html.erb view to render here. Comment out line 23 in the users_controller.rb and comment in the line 24 to see a helpful error message"
+        # render :new
     end
 
     def create
-        @answer = Answer.new(chirp_params)
-        @answer.user_id = current_user.id # TODO fix this for non auth
+        @answer = Answer.new(answer_params)
+        @answer.user_id = 9 # hardcoded for now
+        @answer.question_id = 6 # hardcoded for now
         if @answer.save
-            redirect_to question_url(@answer)
+            redirect_to answer_url(@answer)
         else
             render json: @answer.errors.full_messages, status: 422
         end
@@ -35,13 +37,15 @@ class AnswersController < ApplicationController
 
     def edit
         @answer = Answer.find(params[:id])
-        render :edit
+        @question = Question.find(params[:id])
+        render json: "You need to build an edit.html.erb view to render here. Comment out line 40 in the users_controller.rb and comment in the line 41 to see a helpful error message"
+        # render :edit
     end
 
     def update
         @answer = Answer.find(params[:id])
-        if @answer.update(chirp_params)
-            redirect_to question_url(@answer)
+        if @answer.update(answer_params)
+            redirect_to answer_url(@answer)
         else
             render json: @answer.errors.full_messages, status: 422
         end
@@ -59,6 +63,6 @@ class AnswersController < ApplicationController
     private
 
     def answer_params
-        params.require(:answer).permit(:body, :user_id)
+        params.require(:answer).permit(:body)
     end
 end
